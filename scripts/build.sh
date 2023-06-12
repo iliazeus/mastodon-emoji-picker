7915886c2dbe0822aa5fffcee41055ed813fa94c
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2023-06-12 22:29:24 +0600

    set up project

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+esbuild \
+  --bundle --sourcemap=external --minify --charset=utf8 \
+  --target=es2020 \
+  --format=iife --global-name=EmojiPicker \
+  --outfile=./www/index.js \
+  ./src/index.ts
